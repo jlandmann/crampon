a6d144becf5d4b8fde8b3fea683813108c813837
Johannes Landmann <j_landmann@gmx.de>
2017-03-28 10:23:10 +0200

    Set Up Test Environment for used OGGM stuff

---
+++ crampon/tests/test_graphics.py
@@ -0,0 +1,30 @@
+import unittest
+import warnings
+import pytest
+import os
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+# Local imports
+import crampon.utils
+from crampon.tests import requires_mpltest, requires_internet, \
+    RUN_GRAPHIC_TESTS, init_hef
+from crampon import graphics
+from crampon.core.preprocessing import gis
+import crampon.cfg as cfg
+from crampon.utils import get_demo_file
+from oggm.tests.test_graphics import test_googlemap
+
+# General settings
+warnings.filterwarnings("once", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=UserWarning,
+                        message=r'.*guessing baseline image.*')
+
+# do we event want to run the tests?
+if not RUN_GRAPHIC_TESTS:
+    raise unittest.SkipTest('Skipping all graphic tests.')
+
+# Globals
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+TESTDIR_BASE = os.path.join(CURRENT_DIR, 'tmp')
+
---
+++ crampon/tests/test_utils.py
@@ -0,0 +1,27 @@
+import warnings
+import unittest
+import os
+import shutil
+
+import salem
+import numpy as np
+import pandas as pd
+from numpy.testing import assert_array_equal
+
+from crampon.tests import is_download
+from crampon import utils
+from crampon import cfg
+from oggm.tests.test_utils import TestFuncs, TestInitialize
+
+# General settings
+warnings.filterwarnings("once", category=DeprecationWarning)
+
+# Globals
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+TEST_DIR = os.path.join(CURRENT_DIR, 'tmp_download')
+if not os.path.exists(TEST_DIR):
+    os.makedirs(TEST_DIR)
+
+# I have to change/set up
+#class TestInitialize(unittest.TestCase):
+#TestDataFiles
