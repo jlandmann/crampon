3f3ef3ce59c5685c5af32d2159ad4856c7b5fec7
Johannes Landmann <j_landmann@gmx.de>
2017-05-02 16:14:56 +0200

    Make plot with avilable FOG MB glaciers

--- crampon/graphics.py
+++ crampon/graphics.py
@@ -4,6 +4,12 @@ from crampon.utils import entity_task, global_task
 import logging
 from matplotlib.ticker import NullFormatter
 from oggm.graphics import *
+import os
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+from salem import get_demo_file, DataLevels, GoogleVisibleMap, Map
+import numpy as np
 
 # Local imports
 import crampon.cfg as cfg
@@ -12,3 +18,64 @@ import crampon.cfg as cfg
 log = logging.getLogger(__name__)
 
 nullfmt = NullFormatter()  # no labels
+
+
+def plot_fog_mb_glaciers(fog_dir=None, y=[], x=[]):
+    """
+    Plot available FoG MB glaciers in a certain area.
+    
+    Parameters
+    ----------
+    fog_dir: str
+        Directory where the FoG subfiles are stored. Version 2016-08 should 
+        work.
+    y: list
+        Latitude extent on which to be plotted.
+    x: list
+        Longitude extent on which to be plotted.
+
+    Returns
+    -------
+    
+    """
+    fog_a = pd.read_csv(os.path.join(fog_dir,
+                                     'WGMS-FoG-2016-08-A-GLACIER.csv'),
+                        encoding='iso8859_15')
+    fog_ee = pd.read_csv(os.path.join(fog_dir,
+                                      'WGMS-FoG-2016-08-EE-MASS-BALANCE.csv'),
+                         encoding='iso8859_15')
+
+    ext_ids = fog_a[(fog_a.LONGITUDE >= np.nanmin(x)) &
+                    (fog_a.LONGITUDE <= np.nanmax(x)) &
+                    (fog_a.LATITUDE >= np.nanmin(y)) &
+                    (fog_a.LATITUDE <= np.nanmax(y))].WGMS_ID.values
+
+    # Select and create set of MB glaciers in extent
+    fog_ee = fog_ee[fog_ee.WGMS_ID.isin(ext_ids)]
+    # Where we have yearly measures and no altitude range
+    fog_ee = fog_ee[fog_ee.LOWER_BOUND.isin([9999])]
+
+    # Select back in FOG A what shall be plotted (inefficient!?)
+    fog_a = fog_a[fog_a.WGMS_ID.isin(np.unique(fog_ee.WGMS_ID))]
+
+    # prepare the figure
+    f, ax = plt.subplots()
+
+    g = GoogleVisibleMap(x=[np.nanmin(x), np.nanmax(x)],
+                         y=[np.nanmin(y), np.nanmax(y)],
+                         maptype='satellite')
+
+    # the google static image is a standard rgb image
+    ggl_img = g.get_vardata()
+    sm = Map(g.grid, factor=1, countries=True)
+    sm.set_rgb(ggl_img)  # add background rgb image
+    sm.visualize(ax=ax)
+    x, y = sm.grid.transform(fog_a.LONGITUDE.values, fog_a.LATITUDE.values)
+    ax.scatter(x, y, color='orange', s=50, edgecolors='k', linewidths=1)
+    for i, txt in enumerate(fog_a.NAME.values):
+        ax.annotate(txt, (x[i], y[i]))
+    ax.set_title('Available MB glacier in FoG')
+
+    # make it nice
+    #plt.tight_layout()
+    plt.show()
