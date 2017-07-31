8001a2f9a75c2b339c1927c6695505d6bbe9b671
Johannes Landmann <j_landmann@gmx.de>
2017-07-31 18:34:37 +0200

    avoid init of PastMassBalanceModel

--- crampon/core/models/massbalance.py
+++ crampon/core/models/massbalance.py
@@ -5,14 +5,16 @@ from crampon.cfg import SEC_IN_DAY
 from crampon.utils import SuperclassMeta, lazy_property
 
 
-class DailyMassBalanceModel(PastMassBalanceModel):
+class DailyMassBalanceModel(MassBalanceModel):
 
     def __init__(self, gdir, mu_star=None, bias=None, prcp_fac=None,
                  filename='climate_daily', filesuffix=''):
-        PastMassBalanceModel.__init__(gdir, mu_star=mu_star, bias=bias,
+
+        """
+        PastMassBalanceModel.__init__(self, gdir=gdir, mu_star=mu_star, bias=bias,
                                       prcp_fac=prcp_fac, filename=filename,
                                       filesuffix=filesuffix)
-
+        """
         # should probably be replaced by a direct access to a file that
         # contains uncertainties (don't know how to handle that yet)
         if mu_star is None:
@@ -29,33 +31,44 @@ class DailyMassBalanceModel(PastMassBalanceModel):
             prcp_fac = df['prcp_fac'][0]
         self.mu_star = mu_star
         self.bias = bias
+        self.prcp_fac = prcp_fac
 
         # overwrite/add some OGGM stuff
         self.m = None
         self.years = None
 
+        # Parameters
+        self.t_solid = cfg.PARAMS['temp_all_solid']
+        self.t_liq = cfg.PARAMS['temp_all_liq']
+        self.t_melt = cfg.PARAMS['temp_melt']
+
+        # Public attrs
+        self.temp_bias = 0.
+
+        # Read file
         fpath = gdir.get_filepath(filename, filesuffix=filesuffix)
         with netCDF4.Dataset(fpath, mode='r') as nc:
-            # time
-            time = nc.variables['time']
-            time = netCDF4.num2date(time[:], time.units)
+            # time as nc time variable
+            self.nc_time = nc.variables['time']
+            self.nc_time_units = self.nc_time.units
+            # time as array of datetime instances
+            time = netCDF4.num2date(self.nc_time[:], self.nc_time.units)
 
             # identify the time span
             self.tspan_in = time
-            self.years = self.tspan_in.years
-            self.days_per_year = [len([date for date in self.tspan_in if self.tspan_in.year == y]) for y in self.years]
+            #self.years = np.unique([y.year for y in self.nc_time])
 
             # Read timeseries
             self.temp = nc.variables['temp'][:]
-            self.prcp = nc.variables['prcp'][:] * prcp_fac
+            self.prcp = nc.variables['prcp'][:] * self.prcp_fac
             self.grad = nc.variables['grad'][:]
-
             self.ref_hgt = nc.ref_hgt
 
     def get_daily_mb(self, heights, date=None):
 
         # index of the date of MB calculation
-        ix = netCDF4.date2index(date, self.tspan_in, select='exact')
+        ix = pd.DatetimeIndex(self.tspan_in).get_loc(date)
+        #ix = netCDF4.date2index(date, self.nc_time, select='exact')
 
         # Read timeseries
         itemp = self.temp[ix] + self.temp_bias
