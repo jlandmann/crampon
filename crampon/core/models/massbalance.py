679dcec98f7835d8433cd83e138a3ff35c09c344
Johannes Landmann <j_landmann@gmx.de>
2018-02-26 08:45:16 +0100

    Add simple Braithwaite Model incl. Snow

--- crampon/core/models/massbalance.py
+++ crampon/core/models/massbalance.py
@@ -3,6 +3,8 @@ from __future__ import division
 from oggm.core.massbalance import *
 from crampon.cfg import SEC_IN_DAY
 from crampon.utils import SuperclassMeta, lazy_property
+import xarray as xr
+import cython
 
 
 class DailyMassBalanceModel(MassBalanceModel):
@@ -18,16 +20,16 @@ class DailyMassBalanceModel(MassBalanceModel):
         # should probably be replaced by a direct access to a file that
         # contains uncertainties (don't know how to handle that yet)
         if mu_star is None:
-            df = pd.read_csv(gdir.get_filepath('local_mustar', div_id=0))
+            df = pd.read_csv(gdir.get_filepath('local_mustar'))
             mu_star = df['mu_star'][0]
         if bias is None:
             if cfg.PARAMS['use_bias_for_run']:
-                df = pd.read_csv(gdir.get_filepath('local_mustar', div_id=0))
+                df = pd.read_csv(gdir.get_filepath('local_mustar'))
                 bias = df['bias'][0]
             else:
                 bias = 0.
         if prcp_fac is None:
-            df = pd.read_csv(gdir.get_filepath('local_mustar', div_id=0))
+            df = pd.read_csv(gdir.get_filepath('local_mustar'))
             prcp_fac = df['prcp_fac'][0]
         self.mu_star = mu_star
         self.bias = bias
@@ -41,9 +43,6 @@ class DailyMassBalanceModel(MassBalanceModel):
         self.t_solid = cfg.PARAMS['temp_all_solid']
         self.t_liq = cfg.PARAMS['temp_all_liq']
         self.t_melt = cfg.PARAMS['temp_melt']
-
-        # Public attrs
-        self.temp_bias = 0.
 
         # Read file
         fpath = gdir.get_filepath(filename, filesuffix=filesuffix)
@@ -64,6 +63,9 @@ class DailyMassBalanceModel(MassBalanceModel):
             self.tgrad = nc.variables['grad'][:]
             self.pgrad = cfg.PARAMS['prcp_grad']
             self.ref_hgt = nc.ref_hgt
+
+        # Public attrs
+        self.temp_bias = 0.
 
     def get_daily_mb(self, heights, date=None):
         """
@@ -118,7 +120,8 @@ class DailyMassBalanceModel(MassBalanceModel):
 
         # Compute solid precipitation from total precipitation
         # the prec correction with the gradient does not exist OGGM
-        prcpsol = np.ones(npix) * iprcp + self.pgrad * (heights - self.ref_hgt)
+        prcpsol = np.ones(npix) * iprcp + self.pgrad * iprcp * (heights -
+                                                                self.ref_hgt)
         fac = 1 - (temp - self.t_solid) / (self.t_liq - self.t_solid)
         prcpsol *= np.clip(fac, 0, 1)
 
@@ -131,19 +134,20 @@ class DailyMassBalanceModel(MassBalanceModel):
         return icerate
 
     def get_daily_specific_mb(self, heights, widths, date=None):
-        """Specific mb for a given date and geometry (m w.e. d-1)
+        """Specific mass balance for a given date and geometry (m w.e. d-1).
 
         Parameters
         ----------
         heights: ndarray
-            the altitudes at which the mass-balance will be computed
+            The altitudes at which the mass-balance will be computed.
         widths: ndarray
-            the widths of the flowline (necessary for the weighted average)
-        date: float, optional
-            the time (in the "floating year" convention)
+            The widths of the flowline (necessary for the weighted average).
+        date: datetime.datetime or array of datetime.datetime
+            The date(s) when to calculate the specific mass balance.
+
         Returns
         -------
-        the specific mass-balance (units: mm w.e. d-1)
+        The specific mass-balance of (units: mm w.e. d-1)
         """
         if len(np.atleast_1d(date)) > 1:
             out = [self.get_daily_specific_mb(heights, widths, date=d)
@@ -151,7 +155,176 @@ class DailyMassBalanceModel(MassBalanceModel):
             return np.asarray(out)
 
         # m w.e. d-1
-        mbs = self.get_daily_mb(heights, date=date) * SEC_IN_DAY * cfg.RHO / 1000.
+        mbs = self.get_daily_mb(heights, date=date) * SEC_IN_DAY * cfg.RHO /\
+              1000.
         mbs_wavg = np.average(mbs, weights=widths)
         return mbs_wavg
 
+
+class BraithwaiteModel(DailyMassBalanceModel):
+
+    def __init__(self, gdir, mu_ice=None, mu_snow=None, bias=None,
+                 prcp_fac=None, snow_init=None, filename='climate_daily',
+                 filesuffix=''):
+
+        self.mu_ice = mu_ice
+        self.mu_snow = mu_snow
+        self.prcp_fac = prcp_fac
+        self.bias = bias
+
+        DailyMassBalanceModel.__init__(self, gdir, mu_star=self.mu_ice,
+                                       bias=self.bias, prcp_fac=self.prcp_fac,
+                                       filename='climate_daily', filesuffix='')
+
+        self.heights, self.widths = gdir.get_inversion_flowline_hw()
+        if snow_init:
+            self.snow_init = snow_init
+        else:
+            self.snow_init = np.zeros(
+                [len(self.tspan_in), len(self.heights)])
+        self.snow = np.atleast_2d(np.zeros_like(self.heights))
+
+        # self.snow = xr.Dataset({'swe': (['time', 'h'], self.snow_init)},
+        #                      coords={
+        #                          'h': (['h'], range(len(self.heights))),
+        #                          'time': pd.to_datetime(self.tspan_in)})
+
+    def get_daily_mb(self, heights, date=None):
+        """
+        Calculates the daily mass balance for given heights.
+
+        At the moment the mass balance equation is the simplest formulation:
+
+        MB(z) = PRCP_FAC * PRCP_SOL(z) - mustar * max(T(z) - Tmelt, 0)
+
+        where MB(z) is the mass balance at height z in mm w.e., PRCP_FAC is
+        the precipitation correction factor, PRCP_SOL(z) is the solid
+        precipitation at height z in mm w.e., mustar is the
+        temperature sensitivity of the glacier (mm w.e. K-1 d-1), T(z) is the
+        temperature and height z in (deg C) and Tmelt is the temperature
+        threshold where melt occurs (deg C).
+
+        The result of the model equation is thus a mass balance in mm w.e. d.1,
+        however, for context reasons (dynamical part of the model), the mass
+        balance is returned in m ice s-1, using the ice density as given in the
+        configuration file. ATTENTION, the mass balance given is not yet
+        weighted with the glacier geometry! For this, see method
+        `get_daily_specific_mb`.
+
+        Parameters
+        ----------
+        heights: ndarray
+            Heights at which mass balance should be calculated.
+        date: datetime.datetime
+            Date at which mass balance should be calculated.
+
+        Returns
+        -------
+        ndarray:
+            Glacier mass balance at the respective heights in m ice s-1.
+        """
+
+        # index of the date of MB calculation
+        ix = pd.DatetimeIndex(self.tspan_in).get_loc(date)
+        #ix = netCDF4.date2index(date, self.nc_time, select='exact')
+
+        # Read timeseries
+        itemp = self.temp[ix] + self.temp_bias
+        iprcp = self.prcp[ix]
+        itgrad = self.tgrad[ix]
+
+        # For each height pixel:
+        # Compute temp and tempformelt (temperature above melting threshold)
+        npix = len(heights)
+        temp = np.ones(npix) * itemp + itgrad * (heights - self.ref_hgt)
+        tempformelt = temp - self.t_melt
+        tempformelt[:] = np.clip(tempformelt, 0, tempformelt.max())
+
+        # Compute solid precipitation from total precipitation
+        # the prec correction with the gradient does not exist OGGM
+        prcpsol = np.ones(npix) * iprcp + self.pgrad * iprcp * (heights -
+                                                                self.ref_hgt)
+        fac = 1 - (temp - self.t_solid) / (self.t_liq - self.t_solid)
+        prcpsol *= np.clip(fac, 0, 1)
+
+        # Get snow distribution from yesterday and determine snow/ice from it
+        # One could also get the accumulation first & update snow before melting
+        snowdist = np.where(self.snow[-1] > 0.)
+        mu_comb = np.zeros_like(self.snow[-1])
+        mu_comb[:] = self.mu_ice
+        np.put(mu_comb, snowdist, self.mu_snow)
+
+        # (mm w.e. d-1) = (mm w.e. d-1) - (mm w.e. d-1 K-1) * K - bias
+        mb_day = prcpsol - mu_comb * tempformelt - self.bias
+
+        self.update_snow(date, mb_day)
+
+        # return ((10e-3 kg m-2) w.e. d-1) * (d s-1) * (kg-1 m3) = m ice s-1
+        icerate = mb_day / SEC_IN_DAY / cfg.RHO
+        return icerate
+
+    def update_snow(self, date, mb):
+        """
+        Updates the snow cover on the glacier after a mass balance calculation.
+
+        Parameters
+        ----------
+        date: datetime.datetime
+            The date for which to update the snow cover.
+        mb: array-like
+            Mass balance for elevation bands.
+
+        Returns
+        -------
+        self.snow:
+            The updated snow cover with the reference height, the date when the
+            last fresh snow has fallen on the surface and the amount of snow
+            present.
+        """
+
+        ix = pd.DatetimeIndex(self.tspan_in).get_loc(date)
+
+        snow_today = None
+        #print(mb, self.snow.isel(time=ix-1).swe)
+        # for first day, offset with snow_init
+        if ix > 1:
+            #snow_today = self.snow.isel(time=ix-1).swe + mb
+            #print(date, snow_today)
+            snow_today = np.clip((self.snow[-1,:] + mb), 0, None)
+            self.snow = np.vstack((self.snow, snow_today))
+        elif ix == 0:
+            snow_today = np.clip(mb, 0, None)
+            self.snow = self.snow + snow_today
+        else:
+            snow_today = np.clip(mb, 0, None)
+            self.snow = np.vstack((self.snow, snow_today))
+            #snow_today = self.snow.isel(time=ix).swe + mb
+        #snow_today = np.clip(snow_today, 0, None)
+        #print(self.snow)
+        #self.snow.update(snow_today.to_dataset().expand_dims('time'),
+        #                 inplace=True)
+
+    def get_alpha(self, snow, date):
+        """
+        Calculates the albedo for the snow on the glacier.
+
+        The approach could be after Brock et al. 2000 with values of
+        Pellicciotti et al. 2005 (see Gabbi et al. 2014). However, the sum of
+        maximum temperatures since the last snowfall would be needed (we only
+        have the daily mean so far).
+
+
+        Parameters
+        ----------
+        snow: pd.DataFrame()
+            The glacier's snow characteristics as defined in the ``snow``
+            attribute.
+        date: datetime.datetime
+            Date for which the albedo should be calculated.
+
+        Returns
+        -------
+        ndarray
+            An array with the albedo.
+        """
+        return NotImplementedError
