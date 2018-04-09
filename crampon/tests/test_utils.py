999e4645b3cb1bcab2ec65914c10b94f43e96ead
Johannes Landmann <j_landmann@gmx.de>
2018-04-09 17:27:53 +0200

    Forgot is_download decorator

--- crampon/tests/test_utils.py
+++ crampon/tests/test_utils.py
@@ -6,13 +6,11 @@ import os
 import shutil
 import logging
 
-import salem
-import numpy as np
 import pandas as pd
 import datetime
 from numpy.testing import assert_array_equal, assert_almost_equal
 
-from crampon.tests import requires_credentials, requires_vpn
+from crampon.tests import requires_credentials, requires_vpn, is_download
 from crampon import utils
 from crampon import cfg
 from oggm.tests.test_utils import TestDataFiles as OGGMTestDataFiles
@@ -32,6 +30,7 @@ if not os.path.exists(TEST_DIR):
 
 @requires_credentials
 @requires_vpn
+@is_download
 class TestCirrusClient(unittest.TestCase):
 
     def setUp(self):
@@ -159,9 +158,11 @@ class CramponTestDataFiles(unittest.TestCase):
         utils.mkdir(cfg.PATHS['working_dir'])
         utils.mkdir(cfg.PATHS['tmp_dir'])
 
+    @is_download
     def test_get_oggm_demo_files(self):
         return OGGMTestDataFiles.test_download_demo_files
 
+    @is_download
     def test_get_crampon_demo_file(self):
         # At the moment not implemented
         pass
