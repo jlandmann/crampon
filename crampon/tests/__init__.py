1300da2ba07636fc57765496a8aa635321832b10
Johannes Landmann <j_landmann@gmx.de>
2018-04-10 10:54:08 +0200

    Small bug

--- crampon/tests/__init__.py
+++ crampon/tests/__init__.py
@@ -116,6 +116,7 @@ else:
 
 # check if there is a credentials file (should be added to .gitignore)
 cred_path = os.path.abspath(os.path.join(__file__, "../../..", '.credentials'))
+HAS_CREDENTIALS = False
 if os.path.exists(cred_path):
     HAS_CREDENTIALS = True
     try:
