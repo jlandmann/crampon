cd199cf40f894c6c8f9a108de2469a041e301218
Johannes Landmann <j_landmann@gmx.de>
2018-11-13 16:22:02 +0100

    keep up with OGGM

--- crampon/tasks.py
+++ crampon/tasks.py
@@ -14,24 +14,21 @@ from crampon.core.preprocessing.centerlines import catchment_intersections
 from crampon.core.preprocessing.centerlines import initialize_flowlines
 from crampon.core.preprocessing.centerlines import catchment_width_geom
 from crampon.core.preprocessing.centerlines import catchment_width_correction
-from oggm.core.climate import mu_candidates
+from oggm.core.climate import glacier_mu_candidates
 from oggm.core.climate import process_cru_data
 from crampon.core.preprocessing.climate import process_custom_climate_data
 from oggm.core.climate import process_cesm_data
 
 from oggm.utils import copy_to_basedir
-from oggm.core.climate import apparent_mb
+from oggm.core.climate import apparent_mb_from_linear_mb
 from oggm.core.inversion import prepare_for_inversion
-from oggm.core.inversion import volume_inversion
+from oggm.core.inversion import mass_conservation_inversion
 from oggm.core.inversion import filter_inversion_output
 from oggm.core.inversion import distribute_thickness_interp
 from oggm.core.flowline import init_present_time_glacier
 from oggm.core.flowline import run_random_climate
 
 # Global tasks
-from oggm.core.climate import process_histalp_nonparallel
+from oggm.core.climate import process_histalp_data
 from oggm.core.climate import compute_ref_t_stars
-from oggm.core.climate import distribute_t_stars
-from oggm.core.climate import crossval_t_stars
-
-from oggm.core.inversion import optimize_inversion_params
+from oggm.core.climate import compute_ref_t_stars
