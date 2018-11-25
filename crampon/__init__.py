ba0490454f1c24926ebc2a864230e5725340a823
Johannes Landmann <j_landmann@gmx.de>
2018-11-25 19:07:42 +0100

    Install check no more up to date

--- crampon/__init__.py
+++ crampon/__init__.py
@@ -1,16 +1,6 @@
 from __future__ import absolute_import, division
 import logging
 
-# Check if OGGM is installed
-try:
-    from oggm.version import version as __version__
-except ImportError:  # pragma: no cover
-    raise ImportError('oggm is not properly installed. If you are running '
-                      'from the source directory, please instead create a '
-                      'new virtual environment (using conda or virtualenv) '
-                      'and  then install it in-place by running: '
-                      'pip install -e .')
-
 # Spammers
 logging.getLogger("Fiona").setLevel(logging.WARNING)
 logging.getLogger("shapely").setLevel(logging.WARNING)
