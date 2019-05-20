d0015c24324a6248d99c36891fb11f0324c9b9b7
Johannes Landmann <j_landmann@gmx.de>
2019-05-20 22:37:42 +0200

    overhauled mb clim function

--- operational/climatology_from_daily.py
+++ operational/climatology_from_daily.py
@@ -6,21 +6,20 @@ import logging
 
 # Python imports
 import os
-import glob
 # Libs
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import pandas as pd
 import xarray as xr
 import datetime as dt
-from itertools import product
 # Locals
 import crampon.cfg as cfg
 from crampon import workflow
 from crampon import tasks
 from crampon.workflow import execute_entity_task
 from crampon import graphics, utils
-from crampon.core.models.massbalance import DailyMassBalanceModel, BraithwaiteModel
+from crampon.core.models.massbalance import MassBalance, BraithwaiteModel, \
+    PellicciottiModel, OerlemansModel, SnowFirnCover
 import numpy as np
 
 # Logging options
@@ -30,6 +29,100 @@ logging.basicConfig(format='%(asctime)s: %(name)s: %(message)s',
 log = logging.getLogger(__name__)
 log.setLevel('DEBUG')
 
+
+def make_mb_clim(gdir, mb_model=None, bgyear=1961, endyear=None, write=True,
+                 reset=False):
+    """
+    Make a mass balance climatology for the available calibration period.
+
+    Parameters
+    ----------
+    gdir::py:class:`crampon.GlacierDirectory`
+        The GlacierDirectory to calculate the current mass balance for.
+    mb_model:
+        Which mass balance model to use. Default: None (use all available).
+    bgyear: int
+        Begin year for the climatology. This is automatically cut to the
+        allowed usage span fo the mass balance model (e.g. due to radiation
+        data availability). Default: 1961.
+    endyear: int or None
+        End year for the climatology. Default: None (take the last full mass
+        budget year, i.e. if today's date is within the year is after October
+        1st, then include automatically the recently completed year.
+    write: bool
+        Whether or not to write the result into the GlacierDirectory.
+        Default: True.
+
+    Returns
+    -------
+    mb_ds, snow_cond, time_elap: xr.Dataset, np.ndarray, pd.DatetimeIndex
+        The mass balance as an xarray dataset, the snow conditions during the
+        run and the elapsed time.
+    """
+
+    if mb_model:
+        mb_models = [mb_model]
+    else:
+        mb_models = [eval(m) for m in cfg.MASSBALANCE_MODELS]
+
+    today = dt.datetime.now()
+    if endyear is None:
+        t_month, t_day = today.month, today.day
+        if (t_month >= 10) and (t_day >= 1):
+            endyear = today.year
+        else:
+            endyear = today.year - 1
+
+    heights, widths = gdir.get_inversion_flowline_hw()
+
+    ds_list = []
+    sc_list = []
+    for exp, mbm in enumerate(mb_models):
+        if hasattr(mbm, 'calibration_timespan'):
+            if mbm.calibration_timespan[0]:
+                bgyear = mbm.calibration_timespan[0]
+            if mbm.calibration_timespan[1]:
+                # overwrite endyear
+                endyear = mbm.calibration_timespan[1]
+
+        begin_clim = utils.get_begin_last_flexyear(dt.datetime(bgyear, 12, 31))
+        end_clim = utils.get_begin_last_flexyear(dt.datetime(endyear, 12, 31))
+
+        # copying heights & widths is important, otherwise heights change
+        day_model = mbm(gdir, bias=0.)
+        mb = []
+        run_span = pd.date_range(begin_clim, end_clim)
+        for date in run_span:
+            # Get the mass balance and convert to m per day
+            tmp = day_model.get_daily_specific_mb(heights, widths, date=date)
+            mb.append(tmp)
+
+            # todo: when are clever moments to store the snowcover?
+            if date == end_clim:
+                sc_list.append(day_model.snowcover.to_dataset())
+
+        mb_for_ds = np.moveaxis(np.atleast_3d(np.array(mb)), 1, 0)
+        mb_ds = xr.Dataset({'MB': (['time', 'member', 'model'], mb_for_ds)},
+                           coords={'member': (['member', ],
+                                              np.arange(mb_for_ds.shape[1])),
+                                   'model': (['model', ],
+                                             [day_model.__name__]),
+                                   'time': (['time', ], run_span),
+                                   })
+        ds_list.append(mb_ds)
+
+    # merge all models together
+    merged = xr.merge(ds_list)
+    # todo: units are hard coded and depend on method used above
+    merged.attrs.update({'id': g.rgi_id, 'name': g.name, 'units': 'm w.e.'})
+    if write:
+        merged.mb.append_to_gdir(gdir, 'mb_daily', reset=reset)
+
+    # take care of snow
+    sc_ds = xr.concat(sc_list, dim=pd.Index([m.__name__ for m in mb_models],
+                                            name='model'))
+
+    return merged, sc_ds
 
 # Initialize CRAMPON (and OGGM, hidden in cfg.py)
 cfg.initialize(file='C:\\Users\\Johannes\\Documents\\crampon\\sandbox\\'
