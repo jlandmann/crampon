220d987cf18f698c8a4c18af3fcf5947091b005f
Johannes Landmann <j_landmann@gmx.de>
2019-06-06 17:51:26 +0200

    move radiation processing

--- crampon/core/preprocessing/radiation.py
+++ crampon/core/preprocessing/radiation.py
@@ -13,6 +13,8 @@ import salem
 from crampon import utils
 from rasterio.warp import reproject, Resampling
 from rasterio.merge import merge as merge_tool
+from scipy.interpolate import interp1d
+from crampon import cfg
 
 
 @jit(nopython=True)
@@ -111,8 +113,58 @@ def make_elevation_angle_grid(x, y, xx, yy, dx, dem_array):
     return angle_grid, angle_grid_deg
 
 
-def ipot_loop(mask, resolution, dem_array, azis, azis_rad, altis, altis_rad,
-              times):
+def get_potential_irradiation_without_toposhade(lat_deg, lon_deg, tz='UTC',
+                                                freq='10min'):
+    """
+    Calculate potential irradiation without considering topography.
+
+    We just take 2018 as a dummy year, the frequency at which the potential
+    irradiation is calculated can be determined by the `freq` keyword.
+
+    Parameters
+    ----------
+    lat_deg: float
+        Latitude for which to retrieve the potential irradiation (deg).
+    lon_deg: float
+        Longitude for which to retrieve the potential irradiation (deg).
+    tz: str
+        Time zone.
+    freq: str
+        Frequency at which potential irradiation shall be calculated
+
+    Returns
+    -------
+    alt_azi: pd.Dataframe
+        Dataframe with solar azimuth and altitude (in radians!) as well as the
+        potential irradiation at given time steps.
+    """
+
+    # time zone doesn't matter as we only take daily means
+    # take a dummie year
+    timespan = pd.date_range('2018-01-01 12:00:00', '2018-12-31 23:55:00',
+                             tz=tz, freq=freq)
+    alt_azi = pd.DataFrame(index=timespan,
+                           columns=['altitude_deg', 'azimuth_deg'])
+
+    for m in timespan:
+        # skip if sun is set anyway
+        solar_altitude = get_altitude(lat_deg, lon_deg, m)
+        if (solar_altitude <= 0.) or (np.isnan(solar_altitude)):
+            continue
+
+        alt_azi.loc[m, 'altitude_deg'] = solar_altitude
+        alt_azi.loc[m, 'azimuth_deg'] = get_azimuth(lat_deg, lon_deg, m)
+        alt_azi.loc[m, 'altitude_rad'] = np.deg2rad(
+            alt_azi.loc[m, 'altitude_deg'])
+        alt_azi.loc[m, 'azimuth_rad'] = np.deg2rad(
+            alt_azi.loc[m, 'azimuth_deg'])
+        alt_azi.loc[m, 'ipot'] = radiation.get_radiation_direct(m, alt_azi.loc[
+            m, 'altitude_deg'])
+
+    return alt_azi
+
+
+def ipot_loop(mask, resolution, dem_array, alt_azi):
     """
     Loop over times and all grid pixels to get the potential irradiation.
     
@@ -123,16 +175,15 @@ def ipot_loop(mask, resolution, dem_array, azis, azis_rad, altis, altis_rad,
     Parameters
     ----------
     mask: np.array
-        Mask of the glacier area.
+        Mask of the area of interest.
     resolution: float
         Grid resolution (m).
     dem_array: np.array
         Array of the DEM elevation values.
-    azis:
-    azis_rad:
-    altis:
-    altis_rad:
-    times:
+    alt_azi: pd.Dataframe
+        Dataframe with solar altitudes and azimuths in both radians and
+        degrees. Needs to have columns 'azimuth_deg', 'azimuth_rad',
+        'altitude_deg', altitude_rad' and the index 'time'.
 
     Returns
     -------
@@ -140,6 +191,14 @@ def ipot_loop(mask, resolution, dem_array, azis, azis_rad, altis, altis_rad,
         A grid with potential solar radiation at the given times, corrected
         for slope and elevation of the pixels.
     """
+
+    # delete all-nan rows
+    alt_azi.dropna(inplace=True)
+    azis = alt_azi.azimuth_deg.values.astype(float)
+    azis_rad = alt_azi.azimuth_rad.values.astype(float)
+    altis = alt_azi.altitude_deg.values.astype(float)
+    altis_rad = alt_azi.altitude_rad.values.astype(float)
+    times = [pd.Timestamp(t, tz='UTC') for t in alt_azi.index.values]
 
     x_coords = range(dem_array.shape[1])
     y_coords = range(dem_array.shape[0])
@@ -203,8 +262,10 @@ def ipot_loop(mask, resolution, dem_array, azis, azis_rad, altis, altis_rad,
     for i in np.arange(time_array.shape[2]):
         to_correct = time_array[:, :, i]
         corrected = correct_radiation_for_terrain(to_correct.T, 0., slope,
-                                                  aspect, np.pi / 4. -
+                                                  aspect, np.pi / 2. -
                                                   altis_rad[i], azis_rad[i])
+        # clip zero: diffuse rad is set to zero (direct could be negative!)
+        corrected = np.clip(corrected, 0., None)
         time_array_corrected[:, :, i] = corrected.T
 
     return time_array_corrected
@@ -215,7 +276,7 @@ def correct_radiation_for_terrain(r_beam, r_diff, slope, terrain_a, sun_z,
     """
     Correct radiation on a horizontal surface for terrain slope and azimuth.
 
-    The equation is implemented from [1]_, but sun and terrain azimuth
+    The equation is implemented from [1]_ (eq. 69), but sun and terrain azimuth
     angles are given from true north.
 
     Parameters
@@ -231,7 +292,7 @@ def correct_radiation_for_terrain(r_beam, r_diff, slope, terrain_a, sun_z,
     sun_a: float or array-like
         Sun azimuth angle (radians).
     terrain_a: float or array-like
-        Terrain azimuth in degrees from north.
+        Terrain azimuth in radians from true north.
 
     Returns
     -------
@@ -252,7 +313,7 @@ def correct_radiation_for_terrain(r_beam, r_diff, slope, terrain_a, sun_z,
     return r_s
 
 
-def scale_irradiation_with_potential_irradiation(isis, ipot):
+def scale_irradiation_with_potential_irradiation(sis, ipot):
     """
     Scale the actual measured incoming solar radiation with potential irradiation.
 
@@ -261,7 +322,7 @@ def scale_irradiation_with_potential_irradiation(isis, ipot):
 
     Parameters
     ----------
-    isis: float
+    sis: float
         Mean irradiation over a time span, e.g. one day.
     ipot: np.array
         Grid with the mean potential irradiation over the same time span as
@@ -269,7 +330,7 @@ def scale_irradiation_with_potential_irradiation(isis, ipot):
 
     Returns
     -------
-    isis_distr: np.array
+    sis_distr: np.array
         The solar incoming radiation distributed on terrain.
     """
 
@@ -277,7 +338,7 @@ def scale_irradiation_with_potential_irradiation(isis, ipot):
     ipot_norm = (ipot - np.nanmin(ipot)) / (np.nanmax(ipot) - np.nanmin(ipot))
 
     # Apply normalization to incoming solar radiation
-    isis_distr = isis * ipot_norm
+    isis_distr = sis * ipot_norm
 
     return isis_distr
 
@@ -288,23 +349,87 @@ def test_correct_radiation_for_terrain():
     float_result = correct_radiation_for_terrain(1000., 0., 0., 0., np.pi/2.,
                                                  np.pi)
     # sun in zenith, terrain at 90 deg
-    float_result = correct_radiation_for_terrain(1000., 0., np.pi/4., 0., np.pi/2.,
-                                                 np.pi)
+    float_result = correct_radiation_for_terrain(1000., 0., np.pi / 4., 0.,
+                                                 np.pi / 2., np.pi)
 
 
     # array tests
 
 
+def distribute_ipot_on_flowlines(gdir):
+    """
+    Distribute potential irradiation on a raster grid to the glacier flowlines.
+
+    Parameters
+    ----------
+    gdir: `py:class:crampon.GlacierDirectory`
+        The glacier to process potential irradiation on the flowlines for.
+
+    Returns
+    -------
+    None
+    """
+
+    fls = gdir.read_pickle('inversion_flowlines')
+    catchments = salem.read_shapefile(gdir.get_filepath('flowline_catchments'))
+    ipot_reproj = xr.open_dataset(gdir.get_filepath('ipot'))
+
+    dem = xr.open_rasterio(gdir.get_filepath('dem'))
+    ds = dem.to_dataset(name='data')
+    ds.attrs['pyproj_srs'] = dem.crs
+
+    # Per flowline (important so that later, the indices can be moved)
+    ipot_per_flowline = []
+
+    for fl, c in zip(fls,
+                     [catchments.iloc[i] for i in range(len(catchments))]):
+        # mask all heights in a catchment
+        c_heights = ds.salem.roi(geometry=c.geometry,
+                                 crs=gdir.grid.proj.srs)
+        # use ipot here, not ipot_reproj to not artificially produce values?
+        ipot_subset = ipot_reproj.salem.roi(geometry=c.geometry,
+                                            crs=gdir.grid.proj.srs)
+        fhgt = fl.surface_h
+
+        fl_ipot_arr = np.full((len(fhgt), 365), np.nan, float)
+        for q in range(len(fhgt)):
+            # todo: check if surface surface_h values are lower edge, uppder edge or mid of the interval
+            try:
+                cond = (c_heights.sel(band=1).data.values <= fhgt[q]) & (
+                        c_heights.sel(band=1).data.values > fhgt[q + 1])
+            except IndexError:
+                # trick at the end of the flowline
+                cond = (c_heights.sel(band=1).data.values <= fhgt[q])
+            if not cond.any():
+                continue
+            ts_per_hgt = ipot_subset.where(cond).mean(dim=['x', 'y'],
+                            skipna=True).to_array().values.flatten()
+            fl_ipot_arr[q, :] = ts_per_hgt
+
+        nnan_ix = np.unique(np.where(~np.isnan(fl_ipot_arr))[0])
+        fl_ipot_interp = interp1d(np.arange(len(fhgt))[nnan_ix],
+                                  fl_ipot_arr[nnan_ix, :], axis=0,
+                                  fill_value='extrapolate')(range(len(fhgt)))
+
+        ipot_per_flowline.append(fl_ipot_interp)
+
+    gdir.write_pickle(ipot_per_flowline, 'ipot_per_flowline')
 
 
-if __name__ == '__main__':
+def get_potential_irradiation_with_toposhade(gdir):
+    """
+    Get the potential solar irradiation including topographic shading.
 
-    from crampon import cfg
-    from matplotlib.animation import ArtistAnimation
-    cfg.initialize('c:\\users\\johannes\\documents\\crampon\\sandbox\\CH_params.cfg')
-    t1 = datetime.datetime.now()
+    Parameters
+    ----------
+    gdir: `py_class:crampon.GlacierDirectory`
+        The GlacierDirectory to process the potential solar irradiation for.
 
-    gdir = utils.GlacierDirectory('RGI50-11.B4504', base_dir='c:\\users\\johannes\\documents\\modelruns\\CH\\per_glacier')
+    Returns
+    -------
+    None
+    """
+
     dem = xr.open_rasterio(gdir.get_filepath('dem'))
     with netCDF4.Dataset(gdir.get_filepath('gridded_data'), 'r') as ncd:
         mask = ncd.variables['glacier_mask'][:]
@@ -312,17 +437,14 @@ if __name__ == '__main__':
     ds.attrs['pyproj_srs'] = dem.crs
     grid = salem.grid_from_dataset(ds)
 
-    # assume: grid is small enough that difference in sun angles doesn't matter
-    latitude_deg = np.mean(grid.ll_coordinates[1])  # pos. in the northern h.
-    longitude_deg = np.mean(grid.ll_coordinates[0])  # neg. west from 0 deg
-
     # extended grid to search for sun-blocking terrain
     extend_border = cfg.PARAMS['shading_border']  # meters
     ext_grid = salem.Grid(proj=grid.proj, dxdy=(grid.dx, grid.dy),
                           x0y0=(grid.x0 - extend_border, grid.y0 +
                                 extend_border),
                           nxny=(int(grid.nx + 2 * extend_border / grid.dx),
-                                int(grid.ny + 2 * extend_border / abs(grid.dy))))
+                                int(grid.ny + 2 * extend_border / abs(grid.dy))
+                                ))
     # regrid to coarser resolution (save time)
     ext_regrid = ext_grid.regrid(factor=cfg.PARAMS['reduce_rgrid_resolution'])
     print('new_resolution: {}'.format(ext_regrid.dx))
@@ -370,42 +492,18 @@ if __name__ == '__main__':
     regrid_ds = ext_regrid.to_dataset()
     regrid_ds['height'] = (['y', 'x'], dst_array)
     new_mask = regrid_ds.salem.roi(shape=gdir.get_filepath('outlines'))
-    timespan = pd.date_range('2018-01-01 12:00:00', '2018-12-31 23:55:00',
-                             tz='UTC', freq='1D')  # freq='10min')
-    alt_azi = pd.DataFrame(index=timespan,
-                           columns=['altitude_deg', 'azimuth_deg'])
 
-    for m in timespan:
-        # todo: deal with time zone info :tzinfo=datetime.timezone.utc)
-
-        # skip if sun is set anyway
-        solar_altitude = get_altitude(latitude_deg, longitude_deg, m)
-        if (solar_altitude <= 0.) or (np.isnan(solar_altitude)):
-            continue
-
-        alt_azi.loc[m, 'altitude_deg'] = solar_altitude
-        alt_azi.loc[m, 'azimuth_deg'] = get_azimuth(latitude_deg,
-                                                    longitude_deg, m)
-        alt_azi.loc[m, 'altitude_rad'] = np.deg2rad(
-            alt_azi.loc[m, 'altitude_deg'])
-        alt_azi.loc[m, 'azimuth_rad'] = np.deg2rad(
-            alt_azi.loc[m, 'azimuth_deg'])
-        alt_azi.loc[m, 'ipot'] = radiation.get_radiation_direct(m,
-                                                                alt_azi.loc[m,
-                                                                            'altitude_deg'])
-
-    # delete all-nan rows
-    alt_azi.dropna(inplace=True)
-    azis = alt_azi.azimuth_deg.values.astype(float)
-    azis_rad = alt_azi.azimuth_rad.values.astype(float)
-    altis = alt_azi.altitude_deg.values.astype(float)
-    altis_rad = alt_azi.altitude_rad.values.astype(float)
-    times = [pd.Timestamp(t, tz='UTC') for t in alt_azi.index.values]
+    # assume: grid is small enough that difference in sun angles doesn't matter
+    latitude_deg = np.mean(grid.ll_coordinates[1])  # pos. in the northern h.
+    longitude_deg = np.mean(grid.ll_coordinates[0])  # neg. west from 0 deg
+    freqstr = '10min'
+    alt_azi = get_potential_irradiation_without_toposhade(latitude_deg,
+                                                          longitude_deg,
+                                                          freq=freqstr)
 
     ipot_array = ipot_loop(mask=~np.isnan(new_mask).height.values,
-                           resolution=ext_regrid.dx,
-                           dem_array=dst_array, azis=azis, azis_rad=azis_rad,
-                           altis=altis, altis_rad=altis_rad, times=times)
+                           resolution=ext_regrid.dx, dem_array=dst_array,
+                           alt_azi=alt_azi)
 
     # make daily mean
     group_indices = alt_azi.groupby(by=alt_azi.index.date).indices
@@ -417,67 +515,59 @@ if __name__ == '__main__':
         one_day = ipot_array[:, :, group_indices[pd.Timestamp(day)]]
         # we have to account for time steps during the night
         one_day_mean = np.sum(one_day, axis=2) / (
-                    pd.Timedelta(days=1) / pd.to_timedelta(timespan.freq))
+                    pd.Timedelta(days=1) / pd.to_timedelta(
+                pd.tseries.frequencies.to_offset(freqstr)))
         ipot_array_daymean[:, :, nth_day] = one_day_mean
 
     ipot_ds = ext_regrid.to_dataset()
-    ipot_ds = ipot_ds.assign_coords(time=np.unique(alt_azi.index.date))
+    ipot_ds = ipot_ds.assign_coords(time=np.unique([np.datetime64(d) for d in
+                                                    alt_azi.index.date]))
     ipot_ds['ipot'] = (['x', 'y', 'time'], ipot_array_daymean)
+    ipot_ds.ipot.attrs['pyproj_srs'] = ext_regrid.to_dataset().attrs[
+        'pyproj_srs']
     ipot_ds = ipot_ds.transpose()
     # distribute back to original grid
     ipot_reproj = ds.salem.transform(ipot_ds)
     ipot_reproj.to_netcdf(gdir.get_filepath('ipot'))
 
-    # assign value to flowline
-    # use np.digitize like OGGM does
-    fls = gdir.read_pickle('inversion_flowlines')
-    catchments = salem.read_shapefile(gdir.get_filepath('flowline_catchments'))
 
-    # Param
-    nmin = int(cfg.PARAMS['min_n_per_bin'])
-    smooth_ws = int(cfg.PARAMS['smooth_widths_window_size'])
+if __name__ == '__main__':
 
-    # Per flowline (important so that later, the indices can be moved)
-    catchment_heights = []
-    for ci in catchments:
-        # mask all heights in a catchment
-        c_heights = ds.salem.roi(geometry=ci[1].geometry, crs=gdir.grid.proj.srs)
+    from crampon import cfg
+    from matplotlib.animation import ArtistAnimation
+    cfg.initialize('c:\\users\\johannes\\documents\\crampon\\sandbox\\CH_params.cfg')
+    t1 = datetime.datetime.now()
 
-        # we have to classify the heights in values not smaller than the resolution we used to derive Ipot - otherwise we produce artifical data
+    # done: Gries, Rhone, Silvretta, Findelen
+    bdir = 'c:\\users\\johannes\\documents\\modelruns\\CH\\per_glacier'
+    #gdirs = [utils.GlacierDirectory(id, base_dir=bdir) for id in
+    #         ['RGI50-11.B4312n-1', 'RGI50-11.A55F03', 'RGI50-11.B4504',
+    #          'RGI50-11.B5616n-1', 'RGI50-11.A10G05', 'RGI50-11.C1410']]
+    gdirs = [utils.GlacierDirectory(gid, base_dir=bdir) for gid in
+             ['RGI50-11.A10G05']]
 
-        # get bin indices according to flowline heights
-        # todo: how to get link catchment ID - flowline?
-        fhgt = fl.surface_h
+    from crampon import tasks
+    from crampon.workflow import execute_entity_task
+    task_list = [
+        tasks.glacier_masks,
+        tasks.compute_centerlines,
+        tasks.initialize_flowlines,
+        tasks.compute_downstream_line,
+        tasks.catchment_area,
+        tasks.catchment_intersections,
+        tasks.catchment_width_geom,
+        tasks.catchment_width_correction,
+        #tasks.process_custom_climate_data,
+        tasks.update_climate
 
-        # bin them
-        c_heights_binned = c_heights.groupby_bins('height', fhgt)
+    ]
+    for task in task_list:
+        execute_entity_task(task, gdirs)
 
-        for _, group in list(c_heights_binned):
-            groupmean = group.nanmean()
-
+    for gdir in gdirs:
+        get_potential_irradiation_with_toposhade(gdir)
+        distribute_ipot_on_flowlines(gdir)
 
     t2 = datetime.datetime.now()
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ims = []
-    max_val = np.nanmax(ipot_array)
-    min_val = np.nanmin(ipot_array)
-    valid = np.where(~np.isnan(ipot_array))
-    for i in range(ipot_array.shape[2]):
-        im = ax.imshow(ipot_array[min(valid[0]):max(valid[0]),
-                       min(valid[1]):max(valid[1]),
-                       i].T, aspect='auto',
-                       cmap='gist_ncar', vmin=min_val, vmax=max_val,
-                       animated=True)
-        t = ax.annotate(alt_azi.iloc[i].name.strftime('%m-%d'), (0.01, 0.98),
-                        xycoords='axes fraction', verticalalignment='top',
-                        color='k')
-        if i == 0:
-            fig.colorbar(im)
-        ims.append([im, t])
-    ani = ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
-
-
     print('it took', t2-t1)
     print('hi')
